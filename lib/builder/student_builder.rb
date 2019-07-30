a660a03452425cb26a49b0fca77eb9da86d1d46b
Josh Young <j22cal@gmail.com>
2019-07-30 06:58:40 -0500

    Starting adding an example of the builder pattern

---
+++ lib/builder/student_builder.rb
@@ -0,0 +1,70 @@
+class Student
+  attr_accessor :average
+  attr_accessor :on_campus
+  attr_accessor :hours
+  attr_accessor :courses
+
+  def initialize(average=nil, on_campus=true, meal_plan=MealPlan.new, hours=12, courses=[])
+    @average = average
+    @on_campus = on_campus
+    @meal_plan = meal_plan
+    @hours = hours
+    @courses = courses
+  end
+end
+
+class MealPlan
+  attr_accessor :plan_options
+
+  def initialize(plan_options=[])
+    plan_options = plan_options
+  end
+end
+
+class Course
+  attr_accessor :disipline
+  attr_accessor :current_grade
+  attr_accessor :title
+  attr_accessor :length
+
+  def initialize(disipline="", current_grade=100, title="", length=1)
+    @disipline = disipline
+    @current_grade = current_grade
+    @title = title
+    @length = length
+  end
+end
+
+class Courses
+  attr_accessor :number
+  attr_accessor :within_major
+  attr_accessor :within_minor
+  attr_accessor :course
+
+  def initialize(number=4, within_major=3, within_minor=1, course=Course.new)
+    @number = number
+    @within_major = within_major
+    @within_minor = within_minor
+    @course = cours
+  end
+end
+
+class StudentBuilder
+  attr_accessor :student
+
+  def initialize
+    @student = Student.new
+  end
+
+  def meal_plan
+    @student.meal_plan.options = [1, 2, 3]
+  end
+
+  def add_history_101
+    @student.courses << Course.new(disipline="History", current_grade=100, title="History of Western Civilization 101", length=1)
+  end
+
+  def add_physics_1
+    @student.courses << Course.new(disipline="Science", current_grade=100, title="Physics I", length=3)
+  end
+end
