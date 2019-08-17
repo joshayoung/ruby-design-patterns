a2a2d7fa098e86dda40b8ed5fdc6318259dfd109
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-08-17 15:36:32 -0500

    Move decorators to subfolder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require "pry-byebug"
 require_relative "../lib/singleton.rb"
 require_relative "../lib/builder.rb"
 require_relative "../lib/factory.rb"
-require_relative "../lib/decorator.rb"
+require_relative "../lib/decorators/decorator.rb"
 require_relative "../lib/adapter.rb"
 require_relative "../lib/strategy.rb"
 
