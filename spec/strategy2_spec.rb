a15a8d3864ed9e2762ffb9c5ad15156292a0ca1b
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-09-26 06:57:48 -0500

    WIP: Start the strategy pattern

---
+++ spec/strategy2_spec.rb
@@ -0,0 +1,24 @@
+require_relative "../lib/strategy2.rb"
+
+RSpec.describe Strategy2 do
+  describe "BuildShoe" do
+    it "displays the correct value for tennis shoe" do
+      build_shoe = BuildShoe.new(TennisShoe.new)
+
+      expect(build_shoe.display).to eq "A tennis shoe with black laces"
+    end
+
+    it "displays the correct value for work boot" do
+      build_shoe = BuildShoe.new(WorkBoot.new)
+
+      expect(build_shoe.display).to eq "A work boot with black laces"
+    end
+
+    it "allows the color to be changed" do
+      build_shoe = BuildShoe.new(TennisShoe.new)
+      build_shoe.color = "blue"
+
+      expect(build_shoe.display).to eq "A tennis shoe with blue laces"
+    end
+  end
+end
