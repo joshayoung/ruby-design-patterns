dcb15e52b116422ac792e41127f7df24f9bfa53b
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-10-03 11:35:18 -0500

    Move template to a folder

--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -1,9 +1,9 @@
-require_relative "../lib/template.rb"
+require_relative "../lib/template/template.rb"
 
-RSpec.describe Template do
+RSpec.describe Template::Template do
   describe "TennisShoe" do
     it "raises an error if you call an abstract method in the parent class" do
-      tennis_shoe = TennisShoe.new
+      tennis_shoe = Template::TennisShoe.new
 
       expect { tennis_shoe.build }.to raise_error(StandardError)
     end
@@ -11,7 +11,7 @@ RSpec.describe Template do
 
   describe "DressShoe" do
     it "outputs the shoe correctly" do
-      dress_shoe = DressShoe.new
+      dress_shoe = Template::DressShoe.new
 
       expect(dress_shoe.build).to eq("A black dress shoe")
     end
@@ -19,7 +19,7 @@ RSpec.describe Template do
 
   describe "WorkBoot" do
     it "outputs the shoe correctly" do
-      work_boot = WorkBoot.new
+      work_boot = Template::WorkBoot.new
 
       expect(work_boot.build).to eq("A durable brown work boot shoe with a steel toe")
 
