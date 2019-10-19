a6aa52a7825d3e768a09d9abd631c9bc18e20280
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-10-19 17:19:58 -0500

    Use multiple observers

--- spec/observer_spec.rb
+++ spec/observer_spec.rb
@@ -19,3 +19,38 @@ module Observer
     end
   end
 end
+
+module Observers
+  RSpec.describe Observers do
+    describe "Lever" do
+      it "multiple observers can be added" do
+        power_level = PowerLevel.new
+        gas_consumption = GasConsumption.new
+        tire_wear = TireWear.new
+        lever = Lever.new("Honda Speed")
+        lever.append_observer(power_level)
+        lever.append_observer(gas_consumption)
+        lever.append_observer(tire_wear)
+
+        expect(lever.observers).to include(power_level, gas_consumption, tire_wear)
+      end
+
+      it "updates each observer" do
+        power_level = double(update: double)
+        tire_wear = double(update: double)
+        gas_consumption = double(update: double)
+        allow(PowerLevel).to receive(:new).and_return(power_level)
+        lever = Lever.new("Honda Speed")
+        lever.append_observer(power_level)
+        lever.append_observer(gas_consumption)
+        lever.append_observer(tire_wear)
+
+        lever.change_levels
+
+        expect(power_level).to have_received(:update).with(lever)
+        expect(tire_wear).to have_received(:update).with(lever)
+        expect(gas_consumption).to have_received(:update).with(lever)
+      end
+    end
+  end
+end
